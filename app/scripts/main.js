8ad4c9ab45d46a3c7edc97775b900cdfba4acb1b
MC3D <m.chapman.home@gmail.com>
2014-09-30 12:00:40 -0400

    adds Jakes examples

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,7 +25,7 @@ var ListView = BaseView.extend({
 });
 
 var ItemView = BaseView.extend({
-  tagName: 'li',
+  tagName: 'li class="titles"',
 
   initialize: function(options) {
     this.title = options.title;
@@ -38,46 +38,122 @@ var ItemView = BaseView.extend({
   }
 });
 
-// var listView = new ListView();
-// listView.render();
-//
-// _.each(people, function(person) {
-//   var itemView = new ItemView(person);
-//   itemView.render();
-// });
+var listView = new ListView();
+listView.render();
 
-
-var filterView = new ListView();
-filterView.render();
-
-_.each(movies, function(movie) {
-  var itemView = new ItemView(movie);
-  if($('.input').val() === '')
+_.each(movies, function(title) {
+  var itemView = new ItemView(title);
   itemView.render();
 });
 
-$('.input').keyup(function() {
-  filterView.render();
-  alert( "Handler called." );
 
+// Type window to communicate intent (this example sets a global variable).
+// window.App = {};
+//
+// // This is for view constructors, not instances.
+// App.Views = {};
+//
+// App.Models = {};
+//
+// App.Routers = {};
+//
+// // Routers handle app state; handle necessary instances on the router.
+// App.router = new App.Routers.AppRouter();
+// // Generally signifies code that is too highly dependent on global state; shouldn't really need to do this.
+// App.router.appView = new App.Views.AppView();
 
+(function(){
+    'use strict';
 
-  function filter(element) {
-        var value = $(element).val();
+    window.App = {};
+    App.Views = {};
 
-        $("#theList > li").each(function() {
-            if ($(this).text().search(value) > -1) {
-                $(this).show();
-            }
-            else {
-                $(this).hide();
-            }
+    App.Views.ListView = Backbone.View.extend({
+      initialize: function() {
+        $('body').append(this.el);
+        this.filteredList = this.collection;
+      },
+//need to filter through instead and remove the ones that need to be removed
+      render: function(){
+        this.$el.empty();
+        var self = this;
+        _.each(this.filteredList, function(item){
+          self.$el.append('<li>'+item+'</li>')
         });
-    }
+      },
+        filterBy: function(val){
+          this.filteredList = this.collection.filter(function(item){
+            return item.match(val);
+          });
+          this.render();
+        }
 
+    });
+})();
 
-  // _.filter(itemView.name,$('.input').val())
+$(document).ready(function(){
+  var collection = ['cool', 'rad', 'excellent'];
+  App.listView = new App.Views.ListView({collection: collection});
+  App.listView.render();
 });
+
+$('input[type=search]').on('input', function(){
+  var val = $(this).val();
+  App.listView.filterBy(val);
+})
+
+
+
+
+
+
+
+
+
+
+// var filterView = new ListView();
+// filterView.render();
+//
+// _.each(movies, function(movie) {
+//   var itemView = new ItemView(movie);
+//
+//
+//
+//   filterView.render();
+//
+//   $('.input').on('keyup', function() {
+//     // filterView.render();
+//     // alert( "Handler called." );
+//     var input = $(this).val();
+//     if $('.titles').not(':contains(' + input +')').hide();
+//   });
+//
+// });
+
+
+
+// $('.input').keyup(function() {
+//   filterView.render();
+//   alert( "Handler called." );
+//
+//
+//
+//   function filter(element) {
+//         var value = $(element).val();
+//
+//         $(".list > li").each(function() {
+//             if ($(this).text().search(value) > -1) {
+//                 $(this).show();
+//             }
+//             else {
+//                 $(this).hide();
+//             }
+//         });
+//     }
+//
+//
+//   // _.filter(itemView.name,$('.input').val())
+// });
 
 //
 //   var filter = function() {
